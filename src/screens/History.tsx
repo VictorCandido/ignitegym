51bb7fa53a5dfcf1490d3cd3ced4263909152f5a
Victor Candido <victorev@outlook.com>
2023-11-23 22:50:47 -0300

    Implementação da tela de History

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,10 +1,34 @@
+import HistoryCard from "@components/HistoryCard";
 import ScreenHeader from "@components/ScreenHeader";
-import { VStack } from "native-base";
+import { Heading, SectionList, VStack } from "native-base";
+import { useState } from "react";
 
 export function History() {
+    const [exercises, setExercises] = useState([{
+        title: '26.08.22',
+        data: ['Puxada frontal', 'Remada unilateral']
+    }, {
+        title: '27.08.22',
+        data: ['Puxada frontal']
+    }]);
+
     return (
         <VStack flex={1}>
             <ScreenHeader title='Histórico de Exercícios' />
+
+            <SectionList 
+                sections={exercises}
+                keyExtractor={item => item}
+                px={8}
+                renderSectionHeader={({ section }) => (
+                    <Heading color='gray.200' fontSize='md' mt={10} mb={3}>
+                        { section.title }
+                    </Heading>
+                )}
+                renderItem={({ item }) => (
+                    <HistoryCard />
+                )}          
+            />
         </VStack>
     );
 }
