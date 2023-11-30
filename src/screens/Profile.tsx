e8be50804dc671dea7d1068baaefa1fcc31bd7ae
Victor Candido <victorev@outlook.com>
2023-11-30 18:06:45 -0300

    Finalizado implementação de image picker para mudança de imagem

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -2,14 +2,64 @@ import Button from "@components/Button";
 import Input from "@components/Input";
 import ScreenHeader from "@components/ScreenHeader";
 import UserPhoto from "@components/UserPhoto";
-import { Center, Heading, ScrollView, Skeleton, Text, VStack } from "native-base";
+import { Center, Heading, ScrollView, Skeleton, Text, VStack, useToast } from "native-base";
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 import { useState } from "react";
-import { TouchableOpacity } from 'react-native';
+import { Alert, TouchableOpacity } from 'react-native';
 
 const PHOTO_SIZE = 33;
 
 export function Profile() {
     const [photoIsLoading, setPhotoIsLoading] = useState(false);
+    const [userPhoto, setUserPhoto] = useState('https://github.com/victorcandido.png');
+
+    const toast = useToast();
+
+    async function handleUserPhotoSelect() {
+        setPhotoIsLoading(true);
+
+        try {
+            const photoSelected = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                quality: 1,
+                aspect: [4, 4],
+                allowsEditing: true,
+            });
+    
+            if (photoSelected.canceled) {
+                return;
+            }
+
+            if (photoSelected.assets[0].uri) {
+                const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri, { size: true }) as FileSystem.FileInfo & { size: number };
+
+                console.log(photoInfo)
+
+                if (photoInfo.size && (photoInfo.size / 1024 / 1024) > 5) {
+                    return toast.show({
+                        title: 'Essa imagem é muito grande. Escolha uma de até 5MB.',
+                        placement: 'top',
+                        bgColor: 'red.500',
+                    });
+                }
+
+                setUserPhoto(photoSelected.assets[0].uri);
+            }
+    
+        } catch (error) {
+            console.log('## Falha ao selecionar foto');
+            console.error(error);
+
+            return toast.show({
+                title: 'Falha ao selecionar foto, por favor tente novamente.',
+                placement: 'top',
+                bgColor: 'red.500',
+            });
+        } finally {
+            setPhotoIsLoading(false);
+        }
+    }
     
     return (
         <VStack flex={1}>
@@ -30,13 +80,13 @@ export function Profile() {
                         :
                         
                         <UserPhoto 
-                            source={{ uri: 'https://github.com/victorcandido.png' }}
+                            source={{ uri: userPhoto }}
                             alt="Foto do usuário"
                             size={PHOTO_SIZE}
                         />
                     }
 
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={handleUserPhotoSelect}>
                         <Text 
                             color='green.500' 
                             fontWeight='bold' 
